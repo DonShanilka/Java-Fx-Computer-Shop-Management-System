49be2893ccc22ea6af931102ecf18187b2077159
Shanilka <shanilka799@gmail.com>
2024-01-25 02:10:00 +0530

    EmployeeFxml

--- src/main/java/lk/ijse/Launcher.java
+++ src/main/java/lk/ijse/Launcher.java
@@ -12,7 +12,7 @@ public class Launcher extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/dashBord.fxml"))));
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/employee.fxml"))));
         stage.centerOnScreen();
         stage.setTitle("DashBoard");
 
