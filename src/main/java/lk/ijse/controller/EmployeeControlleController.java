cda0e9e5595339b8562bd8904cf18f2dd2a19b84
Shanilka <shanilka799@gmail.com>
2024-01-25 21:43:07 +0530

    EmployeeClass

--- src/main/java/lk/ijse/controller/EmployeeControlleController.java
+++ src/main/java/lk/ijse/controller/EmployeeControlleController.java
@@ -1,4 +1,17 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import lk.ijse.tm.EmployeeTm;
+
 public class EmployeeControlleController {
+    public TextField txtNic;
+    public TableColumn <?,?> tmNic;
+    public TableView <EmployeeTm> employyeTm;
+
+
+    public void empSaveOnAction(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/lk/ijse/tm/employeeTm.java
+++
@@ -1,4 +0,0 @@
-package lk.ijse.tm;
-
-public class employeeTm {
-}
