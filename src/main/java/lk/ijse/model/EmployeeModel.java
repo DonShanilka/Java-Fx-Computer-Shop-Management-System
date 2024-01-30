59ec6cd5c927952effd1a9743e8206f351893040
Shanilka <shanilka799@gmail.com>
2024-01-30 17:27:13 +0530

    Save Employee

--- src/main/java/lk/ijse/model/EmployeeModel.java
+++ src/main/java/lk/ijse/model/EmployeeModel.java
@@ -19,6 +19,21 @@ public class EmployeeModel {
         PreparedStatement pstm = connection.prepareStatement(sql);
 
         pstm.setString(1, dto.getNic());
+        pstm.setString(2, dto.getName());
+        pstm.setString(3, dto.getAddress());
+        pstm.setString(4, dto.getEmail());
+        pstm.setString(5, dto.getMobile());
+        pstm.setString(6, dto.getBDate());
+        pstm.setString(7, dto.getGender());
+        pstm.setString(8, dto.getNation());
+        pstm.setString(8, dto.getRelation());
+        pstm.setString(10, dto.getEId());
+        pstm.setString(11, dto.getRolle());
+        pstm.setString(12, dto.getJDate());
+        pstm.setString(13, dto.getDepart());
+        pstm.setString(14, dto.getPQ());
+        pstm.setString(15, dto.getExperiance());
+        pstm.setString(16, dto.getUni());
 
 
         return pstm.executeUpdate() > 0;
@@ -36,7 +51,22 @@ public class EmployeeModel {
         ResultSet resultSet = pstm.executeQuery();
         while (resultSet.next()){
             empList.add(new EmployeeDto(
-                    resultSet.getString(1)
+                    resultSet.getString(1),
+                    resultSet.getString(2),
+                    resultSet.getString(3),
+                    resultSet.getString(4),
+                    resultSet.getString(5),
+                    resultSet.getString(6),
+                    resultSet.getString(7),
+                    resultSet.getString(8),
+                    resultSet.getString(9),
+                    resultSet.getString(10),
+                    resultSet.getString(11),
+                    resultSet.getString(12),
+                    resultSet.getString(13),
+                    resultSet.getString(14),
+                    resultSet.getString(15),
+                    resultSet.getString(16)
             ));
         }
 
