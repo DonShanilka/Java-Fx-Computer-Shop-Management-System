5175a41abcc4148070c4a156a3718379bf3def82
Shanilka <shanilka799@gmail.com>
2024-02-21 17:34:29 +0530

    update customer dto

--- src/main/java/lk/ijse/dto/CustomerDto.java
+++ src/main/java/lk/ijse/dto/CustomerDto.java
@@ -1,4 +1,23 @@
 package lk.ijse.dto;
 
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class CustomerDto {
+
+    private String nic;
+    private String name;
+    private String address;
+    private String mobile;
+    private String date;
+    private String paymentMethod;
+    private String paymentType;
+    private String amount;
+    private String item_cat;
+    private String product_name;
+
 }
