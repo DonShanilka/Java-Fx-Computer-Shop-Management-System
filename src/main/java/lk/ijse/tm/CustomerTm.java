bcf4d9c255078f9719bdead3fb9fc985faf3c057
Shanilka <shanilka799@gmail.com>
2024-02-21 17:35:15 +0530

    update customer tm

--- src/main/java/lk/ijse/tm/CustomerTm.java
+++ src/main/java/lk/ijse/tm/CustomerTm.java
@@ -1,4 +1,23 @@
 package lk.ijse.tm;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
 public class CustomerTm {
+
+    private String nic;
+    private String name;
+    private String address;
+    private String mobile;
+    private String date;
+    private String paymentMethod;
+    private String paymentType;
+    private String amount;
+    private String item_cat;
+    private String product_name;
+
 }
