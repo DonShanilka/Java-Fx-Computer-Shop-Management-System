a6420a28f17785f947ddb11db07861c829c5b5ad
Shanilka <shanilka799@gmail.com>
2024-02-27 21:06:37 +0530

    itemTm

--- src/main/java/lk/ijse/tm/ItemTm.java
+++ src/main/java/lk/ijse/tm/ItemTm.java
@@ -1,4 +1,23 @@
 package lk.ijse.tm;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Setter
 public class ItemTm {
+
+    private String id;
+    private String brand;
+    private String modelno;
+    private String year;
+    private String price;
+    private String spec;
+    private String supid;
+    private String date;
+    private String qty;
+    private String type;
+
 }
