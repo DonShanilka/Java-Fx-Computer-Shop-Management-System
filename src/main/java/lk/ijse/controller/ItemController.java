e6f762675705a66dc07a7bc12d79060971eb7b3d
Shanilka <shanilka799@gmail.com>
2024-02-29 19:56:51 +0530

    item controller MonitorDeleteOnAction edit

--- src/main/java/lk/ijse/controller/ItemController.java
+++ src/main/java/lk/ijse/controller/ItemController.java
@@ -1119,7 +1119,21 @@ public class ItemController implements Initializable {
 
     @FXML
     void monitorDeteleOnAction(ActionEvent event) {
+        String id = moniID.getText();
 
+        ItemDto dto = new ItemDto(id);
+
+        try{
+            boolean isDelete = ItemModel.monitorDeteleOnAction(dto);
+
+            if (isDelete){
+                new Alert(Alert.AlertType.CONFIRMATION, "Item is Delete");
+            } else {
+                new Alert(Alert.AlertType.ERROR, "Item is Not Delete");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
