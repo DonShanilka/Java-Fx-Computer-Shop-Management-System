895ddcc36319c26aa6103aa2c185bf0b0a6d6e1b
Shanilka <shanilka799@gmail.com>
2024-02-29 20:01:12 +0530

    item controller UpsDeleteOnAction edit

--- src/main/java/lk/ijse/controller/ItemController.java
+++ src/main/java/lk/ijse/controller/ItemController.java
@@ -1346,7 +1346,21 @@ public class ItemController implements Initializable {
 
     @FXML
     void upsDeleteOnAction(ActionEvent event) {
+        String id = upsId.getText();
 
+        ItemDto dto = new ItemDto(id);
+
+        try{
+            boolean isDelete = ItemModel.upsDeleteOnAction(dto);
+
+            if (isDelete){
+                new Alert(Alert.AlertType.CONFIRMATION, "Item is Delete");
+            } else {
+                new Alert(Alert.AlertType.ERROR, "Item is Not Delete");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
