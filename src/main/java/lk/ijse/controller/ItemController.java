d556f9a600d5674b3831a358a1b6b6a017351306
Shanilka <shanilka799@gmail.com>
2024-03-01 20:27:03 +0530

    other UpdateOnAction

--- src/main/java/lk/ijse/controller/ItemController.java
+++ src/main/java/lk/ijse/controller/ItemController.java
@@ -1340,7 +1340,31 @@ public class ItemController implements Initializable {
 
     @FXML
     void otherUpdateOnAction(ActionEvent event) {
+        String id = otherid.getText();
+        String brand = otherBrand.getText();
+        String modelno = otherModel.getText();
+        String year = odherYear.getText();
+        double price = Double.parseDouble(otherPrice.getText());
+        String spec = otherSpec.getText();
+        String supid = otherSupId.getText();
+        Date date = Date.valueOf(otherDate.getValue());
+        int qty = Integer.parseInt(otherQty.getText());
+        String type = otherType.getValue();
 
+        var dto = new ItemDto(id,brand,modelno,year,price,spec,supid,date,qty,type);
+
+        try {
+            boolean isUpdate = ItemModel.otherUpdateOnAction(dto);
+
+            if (isUpdate){
+                new Alert(Alert.AlertType.CONFIRMATION,"Item is Save .");
+            } else {
+                new Alert(Alert.AlertType.ERROR, "Item is Not Save");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
