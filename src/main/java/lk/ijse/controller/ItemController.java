1d40403b709c5f2a41f0526dcbcb844fe4c87c89
Shanilka <shanilka799@gmail.com>
2024-03-02 11:54:46 +0530

    itemController loadAllacc

--- src/main/java/lk/ijse/controller/ItemController.java
+++ src/main/java/lk/ijse/controller/ItemController.java
@@ -3,18 +3,25 @@ package lk.ijse.controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextArea;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import lk.ijse.dto.CustomerDto;
+import lk.ijse.dto.EmployeeDto;
 import lk.ijse.dto.ItemDto;
 import lk.ijse.model.CustomerModel;
+import lk.ijse.model.EmployeeModel;
 import lk.ijse.model.ItemModel;
+import lk.ijse.tm.EmployeeTm;
+import lk.ijse.tm.ItemTm;
 
 import java.net.URL;
 import java.sql.Date;
 import java.sql.SQLException;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class ItemController implements Initializable {
@@ -68,7 +75,7 @@ public class ItemController implements Initializable {
     private TextField AccYear;
 
     @FXML
-    private TableView<?> AccessoriesTm;
+    private TableView<ItemTm> AccessoriesTm;
 
     @FXML
     private TableView<?> CHtm;
@@ -819,8 +826,8 @@ public class ItemController implements Initializable {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-
     }
+
 
     @FXML
     void accDeleteOnAction(ActionEvent event) {
@@ -874,6 +881,37 @@ public class ItemController implements Initializable {
             throw new RuntimeException(e);
         }
     }
+
+
+    public void loadAllacc(){
+        var model = new ItemModel();
+
+        ObservableList<ItemTm> obList = FXCollections.observableArrayList();
+
+        try{
+            List<ItemDto> dtoList = model.accGetAll();
+
+            for (ItemDto dto : dtoList){
+                obList.add(new ItemTm(
+                        dto.getId(),
+                        dto.getBrand(),
+                        dto.getModelno(),
+                        dto.getYear(),
+                        dto.getPrice(),
+                        dto.getSpec(),
+                        dto.getSupid(),
+                        dto.getDate(),
+                        dto.getQty(),
+                        dto.getType()
+
+                ));
+            }
+            AccessoriesTm.setItems(obList);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
     @FXML
     void chAddOnAction(ActionEvent event) {
