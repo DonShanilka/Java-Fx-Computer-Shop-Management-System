ef5e9c06d09b3a11e62570a48b0f943db3fdf2a8
Shanilka <shanilka799@gmail.com>
2024-03-02 12:29:44 +0530

    itemController loadAllother

--- src/main/java/lk/ijse/controller/ItemController.java
+++ src/main/java/lk/ijse/controller/ItemController.java
@@ -1560,6 +1560,37 @@ public class ItemController implements Initializable {
         }
     }
 
+
+    public void loadAllother(){
+        var model = new ItemModel();
+
+        ObservableList<ItemTm> obList = FXCollections.observableArrayList();
+
+        try{
+            List<ItemDto> dtoList = model.otherGetAll();
+
+            for (ItemDto dto : dtoList){
+                obList.add(new ItemTm(
+                        dto.getId(),
+                        dto.getBrand(),
+                        dto.getModelno(),
+                        dto.getYear(),
+                        dto.getPrice(),
+                        dto.getSpec(),
+                        dto.getSupid(),
+                        dto.getDate(),
+                        dto.getQty(),
+                        dto.getType()
+
+                ));
+            }
+            OtherTM.setItems(obList);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
     @FXML
     void ramAddOnAction(ActionEvent event) {
         String id = txtRamId.getText();
