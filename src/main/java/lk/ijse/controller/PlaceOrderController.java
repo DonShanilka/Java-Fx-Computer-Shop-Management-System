9ae0730321507352083cb2319c91be20557f4b03
Shanilka <shanilka799@gmail.com>
2024-10-18 14:50:52 +0530

    update placeOrder ui and Controller class

--- src/main/java/lk/ijse/controller/PlaceOrderController.java
+++ src/main/java/lk/ijse/controller/PlaceOrderController.java
@@ -1,5 +1,36 @@
 package lk.ijse.controller;
 
-public class PlaceOrderController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
+public class PlaceOrderController {
+    @FXML
+    private Label lblOrderId;
+
+    @FXML
+    private Label lbl_customer_name;
+
+    @FXML
+    private Label lbl_item_name;
+
+    @FXML
+    private Label lbl_item_price;
+
+    @FXML
+    private Label lbl_total_price;
+
+    @FXML
+    private TextField txt_qty;
+
+    @FXML
+    void addToCartOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void clearOnAction(ActionEvent event) {
+
+    }
 }
