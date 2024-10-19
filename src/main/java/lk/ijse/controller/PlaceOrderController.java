2956d4851cf99429152d9f72ae3e0b6e49c92d23
Shanilka <shanilka799@gmail.com>
2024-10-20 00:53:52 +0530

    create lodadCustomerId method

--- src/main/java/lk/ijse/controller/PlaceOrderController.java
+++ src/main/java/lk/ijse/controller/PlaceOrderController.java
@@ -1,13 +1,32 @@
 package lk.ijse.controller;
 
+import com.jfoenix.controls.JFXComboBox;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import lk.ijse.dto.CustomerDto;
+import lk.ijse.model.CustomerModel;
+import lk.ijse.model.ItemModel;
 import lk.ijse.model.OrderModel;
 
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
 public class PlaceOrderController {
+
+    @FXML
+    private JFXComboBox<String> lblCustomerId;
+
+    @FXML
+    private Label lblOrderDate;
+
     @FXML
     private Label lblOrderId;
 
@@ -27,6 +46,8 @@ public class PlaceOrderController {
     private TextField txt_qty;
 
     OrderModel orderModel = new OrderModel();
+    CustomerModel customerModel = new CustomerModel();
+    ItemModel itemModel = new ItemModel();
 
     @FXML
     void addToCartOnAction(ActionEvent event) {
@@ -62,7 +83,37 @@ public class PlaceOrderController {
         }
     }
 
+    private void setData(){
+        lblOrderDate.setText(String.valueOf(LocalDate.now()));
+    }
+
     public void initialize(){
         genarateNextId();
+        setData();
+    }
+
+    public void cmbItemOnAction(ActionEvent actionEvent) {
+    }
+
+    private void lodadCustomerId(){
+        ObservableList<String> obList = FXCollections.observableArrayList();
+
+        try{
+            List<CustomerDto> idList = customerModel.getAll();
+
+            for (CustomerDto dto : idList) {
+                obList.add(dto.getNic());
+            }
+            lblCustomerId.setItems(obList);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void cmbCustomerOnAction(ActionEvent actionEvent) {
+        String id = lblCustomerId.getValue();
+
+
     }
 }
